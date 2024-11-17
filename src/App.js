197cf4bd76ffdfdb5c0ba72d595c0a288a272fb0
Jet Lin <jetlin101@gmail.com>
2024-11-17 13:40:38 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+import logo from './3.JPG';
 import './App.css';
+import Grid from '@mui/material/Grid2';
+import {Button} from '@mui/material'
+import { useState } from 'react';
 
 function App() {
+  const [helloVisable, setHelloVisability] = useState(false);
+  const [dismiss, setDismiss] = useState(false);
+
+  const handleImageClick = () => {
+    setDismiss(true);
+    setHelloVisability(true);
+  };
+
+  const handleCloseClick = () => {
+    setDismiss(false);
+    setHelloVisability(false);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+
+        <Grid className="Griddy" container spacing={2} columns={{ xs: 12 }}>
+          <Grid item size={6}>
+            <Button className="CoolButton" onClick={handleImageClick}>Image</Button>
+          </Grid>
+          <Grid item size={6}>
+            {helloVisable && <img src={logo} className="App-logo" alt="logo" />}
+          </Grid>
+          <Grid item size={6}>
+            {dismiss && <Button className="CoolButton"  onClick={handleCloseClick}>Close</Button>}
+          </Grid>
+        </Grid>
       </header>
     </div>
   );
